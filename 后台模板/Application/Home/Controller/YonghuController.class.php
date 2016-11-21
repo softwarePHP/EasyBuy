488741ae3b9c2d1ec70f5f4ac6c25a450a511efa
zhangjingjing1995 <1005363792@qq.com>
2016-11-21 19:22:07 +0800

    /后台模板 Signed-off-by: zhangjingjing1995 <1005363792@qq.com>

---
+++ 后台模板/Application/Home/Controller/YonghuController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class YonghuController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+    public function delete()
+    {
+        $this->display();
+    }
+    public function view()
+    {
+        $this->display();
+    }
+    public function update()
+    {
+        $this->display();
+    }
+
+}
