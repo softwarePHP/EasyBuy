67e36089a1df8214face72a9a6206604bf38a7ea
zhangyuhan123 <zhangyuhan123top@sina.com>
2016-12-13 10:34:01 +0800

    Signed-off-by: zhangyuhan123 <zhangyuhan123top@sina.com> easybuy2/Application/Home/Controller/OrderController.class.php

--- easybuy2/easybuy2/Application/Home/Controller/OrderController.class.php
+++ easybuy2/easybuy2/Application/Home/Controller/OrderController.class.php
@@ -29,93 +29,112 @@ class OrderController extends Controller
     {
         //购物车中显示商品价格和图片
         session_start();
-        $id=$_SESSION['id'];
-        $condition['userid'] = $id;
-        $shopingcar = M('shopingcar')->where($condition)->select();
-        //dump($shopingcar);
-        $i = 0;
-        foreach ($shopingcar as $vo) {
-            $goodid = $vo['goodid'];
-            $good = M('goods')->find($goodid);
-            $data[$i]['shopid']=$vo['shopid'];
-            $data[$i]['userid']=$vo['userid'];
-            $data[$i]['goodid']=$good['goodid'];
-            $data[$i]['goodname']=$good['goodname'];
-            $data[$i]['goodprice']=$good['goodprice'];
-            $data[$i]['imageurl']=$good['imageurl'];
-            $discount1 = $good['discount'];
-            $discount = $discount1 * 100;
-            $data[$i]['discount']=$discount;
-            $data[$i]['mark1']=$good['mark1'];
-            $data[$i]['mark2']=$good['mark2'];
-            $data[$i]['spec']=$good['spec'];
-            $data[$i]['shopcount']=$vo['shopcount'];
-            /*$goodid = $good['goodid'];
-            $goodname = $good['goodname'];
-            $goodprice = $good['goodprice'];
-            $shopcount = $vo['shopcount'];
-            $date[$i]['goodid']=$goodid;
-            $date[$i]['goodname']=$goodname;
-            $date[$i]['goodprice']=$goodprice;
-            $date[$i]['shopcount']=$shopcount;*/
-            $i++;
-            //dump($good);
-        }
+        if($_SESSION['id']==null)
+        {
+            $this->success('亲，请先登录！','../../../index/login');
 
-        dump($data);
-        $this->assign('data',$data);
-        $this->display();
+        }
+        else {
+
+
+            $id = $_SESSION['id'];
+            $condition['userid'] = $id;
+            $shopingcar = M('shopingcar')->where($condition)->select();
+            //dump($shopingcar);
+            $i = 0;
+            foreach ($shopingcar as $vo) {
+                $goodid = $vo['goodid'];
+                $good = M('goods')->find($goodid);
+                $data[$i]['shopid'] = $vo['shopid'];
+                $data[$i]['userid'] = $vo['userid'];
+                $data[$i]['goodid'] = $good['goodid'];
+                $data[$i]['goodname'] = $good['goodname'];
+                $data[$i]['goodprice'] = $good['goodprice'];
+                $data[$i]['imageurl'] = $good['imageurl'];
+                $discount1 = $good['discount'];
+                $discount = $discount1 * 100;
+                $data[$i]['discount'] = $discount;
+                $data[$i]['mark1'] = $good['mark1'];
+                $data[$i]['mark2'] = $good['mark2'];
+                $data[$i]['spec'] = $good['spec'];
+                $data[$i]['shopcount'] = $vo['shopcount'];
+                /*$goodid = $good['goodid'];
+                $goodname = $good['goodname'];
+                $goodprice = $good['goodprice'];
+                $shopcount = $vo['shopcount'];
+                $date[$i]['goodid']=$goodid;
+                $date[$i]['goodname']=$goodname;
+                $date[$i]['goodprice']=$goodprice;
+                $date[$i]['shopcount']=$shopcount;*/
+                $i++;
+                //dump($good);
+            }
+            //dump($data);
+            $women = M('grand')->where("mark=1")->select();
+            $men = M('grand')->where("mark=2")->select();
+            $children = M('grand')->where("mark=3")->select();
+            $this->assign('women',$women);
+            $this->assign('men',$men);
+            $this->assign('children',$children);
+            $this->assign('data', $data);
+            $this->assign('user',$_SESSION['user']);
+            $this->assign('logout','退出');
+            $this->display();
+        }
     }
 
     public function order(){
 
 
 
-            //实例化对象order
+        //实例化对象order
 
-            //$orders = $orderTable->where($orderTable['userid']=$_SESSION['id'])->select();
-            //实例化对象goods
-            //$goodsTable = M('goods');
+        //$orders = $orderTable->where($orderTable['userid']=$_SESSION['id'])->select();
+        //实例化对象goods
+        //$goodsTable = M('goods');
 
-            //$goods = $goodsTable->where($goodsTable['goodid']=$orderTable['goodid'])->select();
-            //实例化订单物品
-            //$orderdate = M('orderdate');
-            //$orderid = $orderTable['orderid'];
-            //$orderdates = $orderdate ->query('select * from orderdate where orderid = '.$orderid);
-            //$orderdates = $orderdate->where($orderdate['orderid']=$orderid)->select();
-            //dump($orderdates);
-            //dump($goods);
+        //$goods = $goodsTable->where($goodsTable['goodid']=$orderTable['goodid'])->select();
+        //实例化订单物品
+        //$orderdate = M('orderdate');
+        //$orderid = $orderTable['orderid'];
+        //$orderdates = $orderdate ->query('select * from orderdate where orderid = '.$orderid);
+        //$orderdates = $orderdate->where($orderdate['orderid']=$orderid)->select();
+        //dump($orderdates);
+        //dump($goods);
 
-            //$orders = $orderTable ->query('select * from orderdate where orderid = '.$orderid);
+        //$orders = $orderTable ->query('select * from orderdate where orderid = '.$orderid);
 
-            //$data = $easybuy->OrderSelect($orders);
-            //dump($data);
-            /*$easybuy = new easybuy();
-            $orderTable = M('orders');
-            $orders = $orderTable->select();
-            $data = $easybuy->OrderSelect($orders);
-            //$this->assign('order', $orders);
-            $this->assign('order',$data);
-            //dump($orders);
-            dump($data);
-            $this->display();*/
+        //$data = $easybuy->OrderSelect($orders);
+        //dump($data);
+        /*$easybuy = new easybuy();
+        $orderTable = M('orders');
+        $orders = $orderTable->select();
+        $data = $easybuy->OrderSelect($orders);
+        //$this->assign('order', $orders);
+        $this->assign('order',$data);
+        //dump($orders);
+        dump($data);
+        $this->display();*/
+        //$id=I('userid');
+        $shopid=I('shopid');
         session_start();
         $id=$_SESSION['id'];
-        $shopid=I('shopid');
         $condition['userid']=$id;
         $condition['shopid']=$shopid;
+        $shopids = $_POST['shopids'];
+        //dump($shopids);
         //$shopids=array();
         //$shopids[]=$_POST('goods[]');
         //dump($shopids);
-        //$j = 0;
+        $j = 0;
         $i=0;
-        //foreach ($shopids as $valus){
-            //$condition['userid']=$id;
-            //$condition['shopid']=$valus[$j];
-            $shopingcar=M('shopingcar')->where($condition)->select();
-            dump($condition);
-            dump($shopingcar);
-           //$j++;
+        foreach ($shopids as $valus){
+            $condition1['userid']=3;
+            $condition1['shopid']=$valus[$j];
+            $shopingcar=M('shopingcar')->where($condition1)->select();
+            //dump($condition1);
+            //dump($shopingcar);
+
             foreach($shopingcar as $vo){
                 $goodid=$vo['goodid'];
                 $good=M('goods')->find($goodid);
@@ -129,7 +148,7 @@ class OrderController extends Controller
                 $goodprice = $good['goodprice'];
                 $count = $vo['shopcount'];
                 $total =  $goodprice * $count;
-                //$alltotal = $alltotal + $total;
+
                 $data[$i]['total'] = $total;
                 $data[$i]['discount']=$discount;
                 $data[$i]['goodprice']=$goodprice;
@@ -141,37 +160,47 @@ class OrderController extends Controller
                 $i++;
                 //dump($good);
             }
-            $j = 0;
-            $ids['userid']=$id;
-            $address=M('adress')->where($ids)->select();
-            foreach ($address as $value){
-                $userid = $value['userid'];
-                $user=M('user')->find($userid);
-                $values[$j]['username']=$user['username'];
-                $values[$j]['tel']=$value['tel'];
-                $values[$j]['adress']=$value['adress'];
-                $j++;
-            }
-            $adress = $_GET['adress'];
-        dump($adress);
 
-        dump($values);
-        //}
-        dump($data);
+            //$adress = $_GET['adress'];
+            //dump($adress);
+
+            //dump($values);
+        }
+        $j = 0;
+        $ids['userid']=3;
+        $address=M('adress')->where($ids)->select();
+        foreach ($address as $value){
+            $userid = $value['userid'];
+            $user=M('user')->find($userid);
+            $values[$j]['username']=$user['username'];
+            $values[$j]['tel']=$value['tel'];
+            $values[$j]['adress']=$value['adress'];
+            $j++;
+        }
+        //dump($data);
+        $women = M('grand')->where("mark=1")->select();
+        $men = M('grand')->where("mark=2")->select();
+        $children = M('grand')->where("mark=3")->select();
+        $this->assign('women',$women);
+        $this->assign('men',$men);
+        $this->assign('children',$children);
         $this->assign('values',$values);
         $this->assign('data',$data);
         $this->display();
 
     }
     public function over(){
+
         $this->display();
     }
     public function payment(){
         session_start();
         $id=$_SESSION['id'];
+        //dump($id);
         $shopid=I('shopid');
         $condition['userid']=$id;
         $condition['shopid']=$shopid;
+        $condition1['userid']=$id;
         $shopingcar=M('shopingcar')->where($condition)->select();
         $orderTable = M('orders');
         /*$i = 0;
@@ -182,7 +211,6 @@ class OrderController extends Controller
             $data = array();
             $data[$i]['userid'] = $id;
             $data[$i]['shopid'] = $vo['shopid'];
-            $data[$i]['userid'] = $vo['userid'];
             $data[$i]['ordernumber'] = rand(100000000,999999999);
             $data[$i]['ordertime'] = time();
             $data[$i]['discount'] = $good['discount'];
@@ -192,24 +220,39 @@ class OrderController extends Controller
         }*/
         $goodid = $shopingcar['goodid'];
         $good = M('goods')->find($goodid);
-        $condition1=array();
-        $condition1['userid']=$id;
-        $address = M('adress')->where($condition1)->select();
+        $address = M('adress')->where($condition1)->find();
         $data = array();
         $data['userid'] = $id;
-        dump($data['userid']);
+        //dump($data['userid']);
         $data['shopid'] = $shopingcar['shopid'];
         $data['ordernumber'] = rand(100000000,999999999);
-        $data['ordertime'] = time();
+        $date = time();
+        $ordertime = date('Y-m-d H:m:s',$date);
+        $data['ordertime'] = $ordertime;
         $data['discount'] = $good['discount'];
+        //dump($good['discount']);
         $data['orderaddress'] = $address['adress'];
         $data['orderstate'] = 2;
-        $result = $orderTable->add($data);
-        if ($result){
-            $this->success('插入成功');
-        } else{
-            $this->error('插入失败');
-        }
+        $orderTable->add($data);
+        //dump($result1);
+        /*$orderdateTable = M('orderdate');
+        $shopingcar=M('shopingcar')->where($condition)->select();
+        $data1=array();
+        $data1['goodid']=$good['goodid'];
+        $data1['orderid']=$orderTable['orderid'];
+        $data1['count']=$shopingcar['count'];
+        $orderdateTable->add($data1);*/
+
+        /*$goodprice = $good['goodprice'];
+        $count = $shopingcar['count'];
+        $total =  $goodprice * $count;
+        $data2['total']=$total;*/
+        $women = M('grand')->where("mark=1")->select();
+        $men = M('grand')->where("mark=2")->select();
+        $children = M('grand')->where("mark=3")->select();
+        $this->assign('women',$women);
+        $this->assign('men',$men);
+        $this->assign('children',$children);
         $this->assign('data',$data);
         $this->display();
     }
@@ -217,4 +260,5 @@ class OrderController extends Controller
 
 
     }
+
 }
